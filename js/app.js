d240e1c3f1926fe5eafdafdc0d7c0a18a7bad00e
nagisa-oda <andisan3434@gmail.com>
2026-04-07 12:00:18 +0900

    logDate関数を変更

--- js/app.js
+++ js/app.js
@@ -46,12 +46,11 @@ function switchPage(pageName) {
 }
 
 function logDate(timestamp) {
-    const now = new Date(timestamp); // タイムスタンプから日付を取得
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0'); // ゼロ埋め(この場合はゼロが最大で二つ)
-    const day = String(now.getDate()).padStart(2, '0');
-    return `${year}/${month}/${day}`;
-
+   const date = timestamp.toDate ? timestamp.toDate() : new Date(timestamp);
+   const year = date.getFullyear();
+   const month = String(date.getMonth() + 1).padStart(2, '0');
+   const day = String(date.getDate()).padStart(2, '0');
+   return `${year}/${month}/${day}`;
 }
 
 // フォームのリセット処理
